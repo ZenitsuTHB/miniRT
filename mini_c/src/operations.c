bee8ab36a6555c870ce4edde47f5678103089a6e
Archly Volcy <starxyz12@gmail.com>
2024-08-13 21:16:56 +0200

    test

--- mini_c/src/operations.c
+++ mini_c/src/operations.c
@@ -6,11 +6,18 @@
 /*   By: avolcy <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 19:00:11 by avolcy            #+#    #+#             */
-/*   Updated: 2024/08/13 19:32:49 by avolcy           ###   ########.fr       */
+/*   Updated: 2024/08/13 20:13:20 by avolcy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vector.h"
+
+void init_structs(t_vector *vector)
+{
+	vector->x = 0;
+	vector->y = 0;
+	vector->z = 0;
+}
 
 t_vector* add_vec(t_vector* result, const t_vector* v)
 {
@@ -20,3 +27,19 @@ t_vector* add_vec(t_vector* result, const t_vector* v)
 
 	return  (result);
 }
+
+t_vector* mult_vec(t_vector* result, const t_vector* v)
+{
+	result->x *= v->x;
+	result->y *= v->y;
+	result->z *= v->z;
+
+	return  (result);
+}
+
+double vector_get(t_vector* v, int i)
+{
+	return ();
+}
+
+double vector_set(){}
