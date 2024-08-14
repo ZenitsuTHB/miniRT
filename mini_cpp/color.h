78301e71281833392f03c2dc35faeb5bced44a50
Archly Volcy <starxyz12@gmail.com>
2024-08-14 20:54:19 +0200

    for libs

--- mini_cpp/color.h
+++ mini_cpp/color.h
@@ -12,8 +12,8 @@ void write_color(std::ostream& out, const color& pixel_color) {
     auto b = pixel_color.z();
 
     // Translate the [0,1] component values to the byte range [0,255].
-    int rbyte = int(255.999 * r);
-    int gbyte = int(255.999 * g);
+    int rbyte = int(2.999 * r);
+    int gbyte = int(200.999 * g);
     int bbyte = int(255.999 * b);
 
     // Write out the pixel color components.
--- mini_cpp/main.cpp
+++ mini_cpp/main.cpp
@@ -12,7 +12,7 @@ int main() {
 
     // Image
  	auto aspect_ratio = 16.0 / 9.0;
-    int image_width = 400;
+    int image_width = 1400;
 
     // Calculate the image height, and ensure that it's at least 1.
     int image_height = int(image_width / aspect_ratio);
