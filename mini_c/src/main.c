f24b58021af3c2d10599df25ea41da0071c75115
ZenitsuTHB <starxyz12@gmail.com>
2024-08-16 23:22:54 +0200

    Functions to solve the subject

--- mini_c/src/main.c
+++ mini_c/src/main.c
@@ -5,9 +5,11 @@ int main(void)
 {
   t_win init;
 
+  init.mlx_connection = NULL;
   init.mlx_connection = mlx_init(HEIGHT, WIDTH, "miniRT", true);
   if (init.mlx_connection == NULL)
     return (1);
+  //bool mlx_loop_hook(mlx_t* mlx, void (*f)(void*), void* param)
   mlx_loop(init.mlx_connection);
   mlx_terminate(init.mlx_connection);
   free(init.mlx_connection);
