fac2b9a8036a1f10396779a8e57b3aa60ac9c249
ZenitsuTHB <starxyz12@gmail.com>
2024-08-16 23:23:41 +0200

    ctrl + s

--- mini_c/src/main.c
+++ mini_c/src/main.c
@@ -10,6 +10,12 @@ int main(void)
   if (init.mlx_connection == NULL)
     return (1);
   //bool mlx_loop_hook(mlx_t* mlx, void (*f)(void*), void* param)
+  //static void mlx_render_images(mlx_t* mlx)
+  //void mlx_close_window(mlx_t* mlx)
+  //void mlx_delete_image(mlx_t* mlx, mlx_image_t* image)
+  //mlx_image_t* mlx_new_image(mlx_t* mlx, uint32_t width, uint32_t height)
+  //int32_t mlx_image_to_window(mlx_t* mlx, mlx_image_t* img, int32_t x, int32_t y)
+  //void mlx_set_instance_depth(mlx_instance_t* instance, int32_t zdepth)
   mlx_loop(init.mlx_connection);
   mlx_terminate(init.mlx_connection);
   free(init.mlx_connection);
