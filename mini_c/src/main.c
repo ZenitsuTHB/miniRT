b07225594dd3d65ba73842e39e443ce15a0706e3
ZenitsuTHB <starxyz12@gmail.com>
2024-08-16 18:16:36 +0200

    just a little window

--- mini_c/src/main.c
+++ mini_c/src/main.c
@@ -1,10 +1,17 @@
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <minirt.h>
 
 int main(void)
 {
+  t_win init;
 
-	printf("\n\nHALLO miniRT\n\n");
+  init.mlx_connection = mlx_init(HEIGHT, WIDTH, "miniRT", true);
+  if (init.mlx_connection == NULL)
+    return (1);
+  mlx_loop(init.mlx_connection);
+  mlx_terminate(init.mlx_connection);
+  free(init.mlx_connection);
+
+	//printf("\n\nHALLO miniRT\n\n");
 	return (EXIT_SUCCESS);
 }
