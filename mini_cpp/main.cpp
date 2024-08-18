9aaa666086adf4d4ddcea038a0e44368208c0222
Archly Volcy <starxyz12@gmail.com>
2024-08-18 15:08:18 +0200

    to have the sphere

--- mini_cpp/main.cpp
+++ mini_cpp/main.cpp
@@ -1,17 +1,30 @@
-#include <iostream>
 #include "color.h"
 #include "ray.h"
 #include "vector.hpp"
 
-color ray_color(const ray& r)
-{
-	return color(0,110,255);
+#include <iostream>
+
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+    return (discriminant >= 0);
+}
+color ray_color(const ray& r) {
+    if (hit_sphere(point3(0,0,-1), 0.5, r))
+		return color(1, 0, 0);
+	vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
 }
 
 int main() {
 
     // Image
- 	auto aspect_ratio = 16.0 / 9.0;
+
+    auto aspect_ratio = 16.0 / 9.0;
     int image_width = 1400;
 
     // Calculate the image height, and ensure that it's at least 1.
@@ -37,24 +50,22 @@ int main() {
     auto viewport_upper_left = camera_center
                              - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
     auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
-    
+
     // Render
 
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
-    for (int j = 0; j < image_height; j++) 
-	{
-		std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-
-        for (int i = 0; i < image_width; i++)
-		{
-			auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-        	auto ray_direction = pixel_center - camera_center;
+    for (int j = 0; j < image_height; j++) {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+        for (int i = 0; i < image_width; i++) {
+            auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+            auto ray_direction = pixel_center - camera_center;
             ray r(camera_center, ray_direction);
 
             color pixel_color = ray_color(r);
-            write_color(std::cout, pixel_color);  
+            write_color(std::cout, pixel_color);
         }
     }
-    std::clog << "\rDone.                \n";
+
+    std::clog << "\rDone.                 \n";
 }
