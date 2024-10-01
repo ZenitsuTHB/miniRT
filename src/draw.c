6f2f3002c5c4d8387e5017b63102932ade2f3023
Adrià Márquez Nebot <adrmarqu@car15s2.42barcelona.com>
2024-10-01 12:26:36 +0200

    draw

--- src/draw.c
+++ src/draw.c
@@ -6,7 +6,7 @@
 /*   By: adrmarqu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 17:56:54 by adrmarqu          #+#    #+#             */
-/*   Updated: 2024/09/30 18:21:47 by adrmarqu         ###   ########.fr       */
+/*   Updated: 2024/10/01 11:58:07 by adrmarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,9 @@
 
 //mlx_put_pixel(scene->img, x, y, color);
 
+void	draw_cylinder
+
 void	draw_scene(t_scene *scene)
 {
-	printf("%lf\n", scene->spheres->radious);
-	//draw_cylinder();
+	draw_cylinder(scene->cylinders);
 }
