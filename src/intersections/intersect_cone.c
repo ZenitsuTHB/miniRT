c76e9a565aa13a30b8e8fb174a82b47e15893b63
Adrià Márquez Nebot <adrmarqu@car7s2.42barcelona.com>
2024-10-28 13:52:47 +0100

    Intersection of cone a medias

--- src/intersections/intersect_cone.c
+++ src/intersections/intersect_cone.c
@@ -6,62 +6,106 @@
 /*   By: adrmarqu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 11:51:00 by adrmarqu          #+#    #+#             */
-/*   Updated: 2024/10/27 12:16:24 by adrmarqu         ###   ########.fr       */
+/*   Updated: 2024/10/28 13:49:05 by adrmarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minirt.h"
 
+int	calculate_abcd(t_operation *op, t_cone *co, t_vec3 dir, t_vec3 cyo)
+{
+	double	a;
+	double	b;
+	double	c;
+
+	a = dot_product(&dir, &dir);
+	b = pow(dot_product(&dir, &co->normal), 2);
+	op->A = a - b;
+	a = dot_product(&dir, &cyo);
+	b = dot_product(&dir, &co->normal);
+	c = dot_product(&cyo, &co->normal);
+	op->B = 2 * (a - b * c);
+	a = dot_product(&cyo, &cyo);
+	b = pow(dot_product(&cyo, &co->normal), 2);
+	c = pow(co->radius, 2);
+	op->C = a - b - c;
+	op->delta = op->B * op->B - 4 * op->A * op->C;
+	if (op->delta < 0)
+		return (1);
+	return (0);
+}
+
+void	calculate_t(t_operation *op)
+{
+	double	tmp;
+
+	op->t[0] = (-op->B - sqrt(op->delta)) / (2 * op->A);
+	op->t[1] = (-op->B + sqrt(op->delta)) / (2 * op->A);
+	if (op->t[0] > op->t[1])
+	{
+		tmp = op->t[0];
+		op->t[0] = op->t[1];
+		op->t[1] = tmp;
+	}
+}
+
+t_vec3	get_cone_normal(t_vec3 hp, t_cone *co, double m)
+{
+	t_vec3	a;
+	t_vec3	b;
+	t_vec3	c;
+
+	a = scalar_mult(co->normal, m);
+	b = add_vec3(co->pos, a);
+	c = substract_vec3(hp, b);
+	return (unit_vec3(c));
+}
+
+int	check_interaction(t_ray *ray, t_operation op, t_cone *co, int i)
+{
+	t_vec3	hp;
+	double	a;
+	double	b;
+	double	m;
+
+	if (op.t[i] > 0)
+	{
+		hp = add_vec3(op.tri.origin, scalar_mult(op.tri.dir, op.t[i]));
+		a = dot_product(&op.tri.dir, &co->normal);
+		b = dot_product(&op.tri.co, &co->normal);
+		m = a * op.t[i] + b;
+		if (m >= 0 && m <= co->height)
+		{
+			ray->hit = true;
+			ray->distance = op.t[i];
+			ray->hit_point = hp;
+			ray->normal = get_cone_normal(hp, co, m);
+			return (1);
+		}
+	}
+	return (0);
+}
+
 t_ray	hit_cone(t_vec3 dir, t_vec3 origin, t_cone *co)
 {
 	t_ray		ray;
 	t_operation	op;
-	t_vec3		axis;
-	t_vec3		hitcone;
-	double		tan;
-	double		dot_axis[2];
-	double		sqrt_delta;
-	double		t;
-	double		height;
+	int			i;
 
-	printf("Pintar CO\n");
 	ray.hit = false;
-
-	op.OC = substract_vec3(origin, co->pos);
-	axis = unit_vec3(co->normal);
-
-	tan = (co->radius / co->height) * (co->radius / co->height);
-	
-	dot_axis[0] = dot_product(&dir, &axis);
-	dot_axis[1] = dot_product(&op.OC, &axis);
-
-	op.A = dot_product(&dir, &dir) - (1 + tan) * dot_axis[0] * dot_axis[0];
-	op.B = 2 * dot_product(&dir, &op.OC) - (1 + tan) * dot_axis[0] * dot_axis[1];
-	op.C = dot_product(&op.OC, &op.OC) - (1 + tan) * dot_axis[1] * dot_axis[1];
-
-	op.delta = op.B * op.B - 4 * op.A * op.C;
-	if (op.delta < 0)
+	ray.distance = INFINITY;
+	op.tri.dir = dir;
+	op.tri.origin = origin;
+	op.tri.co = substract_vec3(co->pos, origin);
+	if (calculate_abcd(&op, co, dir, op.tri.co))
 		return (ray);
-
-	sqrt_delta = sqrt(op.delta);
-	op.t[0] = (-op.B - sqrt_delta) / (2 * op.A);
-	op.t[1] = (-op.B + sqrt_delta) / (2 * op.A);
-
-	t = (op.t[0] > 0) ? op.t[0] : ((op.t[1] > 0) ? op.t[1] : -1);
-	if (t < 0)
-		return (ray);
-
-	ray.hit_point = add_vec3(origin, scalar_mult(dir, t));
-	hitcone = substract_vec3(ray.hit_point, co->pos);
-	height = dot_product(&hitcone, &axis);
-
-	if (height < 0 || height > co->height)
-		return (ray);
-
-	printf("Pintado CO\n");
-	ray.hit = true;
-	ray.distance = t;
-	ray.normal = unit_vec3(substract_vec3(hitcone, scalar_mult(axis, height * (1 + tan))));
-
+	calculate_t(&op);
+	i = 0;
+	while (i < 2)
+	{
+		if (check_interaction(&ray, op, co, i))
+			return (ray);
+		i++;
+	}
 	return (ray);
 }
