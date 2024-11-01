47b87df61028e935eac4ebf0853a5a6cb10fe91e
ZenitsuTHB <starxyz12@gmail.com>
2024-11-01 12:30:36 +0100

    testing on Planes

--- src/render/phong_light.c
+++ src/render/phong_light.c
@@ -6,7 +6,7 @@
 /*   By: avolcy <avolcy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 18:46:30 by avolcy            #+#    #+#             */
-/*   Updated: 2024/11/01 02:56:29 by avolcy           ###   ########.fr       */
+/*   Updated: 2024/11/01 11:25:20 by avolcy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,7 +68,7 @@ t_rgb get_specular_color(t_obj *obj, t_light *light, t_vec3 point, t_camera *cam
     view_dir = unit_vec3(substract_vec3(point, cam->origin));
     light_dir = unit_vec3(substract_vec3(point, light->pos));
     reflect_dir = reflect_vec(light_dir, normal);
-    spec = pow(fmax(dot_product(&view_dir, &reflect_dir), 0.0), 255.9);
+    spec = pow(fmax(dot_product(&view_dir, &reflect_dir), 0.0), 32.9);
     return scalar_mult(light->color, light->bright * spec);
 }
 
